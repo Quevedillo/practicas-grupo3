4669523f7ced1298afb3188ba36b97be607ecf54
eryoyui <franvalenciadampc@gmail.com>
2025-03-27 09:12:31 +0100

    cambio login

--- practicas/loginUsuario.php
+++ practicas/loginUsuario.php
@@ -16,12 +16,16 @@
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-4">
+                <img href="logo" alt="logo" src="./logo.png">
                 <h3 class="text-center">Iniciar Sesión</h3>
                 <form action="backend/login.php" method="POST">
-                    <input type="email" name="correo" class="form-control my-2" placeholder="Correo" required>
-                    <input type="password" name="contraseña" class="form-control my-2" placeholder="Contraseña" required>
-                    <button type="submit" class="btn btn-primary w-100">Ingresar</button>
-                    <a href="#" class="d-block text-center mt-2" onclick="toggleRecuperar()">¿Olvidaste tu contraseña?</a>
+                    <label>Usuario:</label>
+                    <input type="user" name="usuario" class="form-control my-2" required>
+                    <label>Contraseña:</label>
+                    <input type="password" name="contraseña" class="form-control my-2" required>
+                    <button type="submit" class="btn btn-primary w-100">Iniciar Sesión</button>
+                    <a href="#" class="d-block text-center mt-2" onclick="toggleRecuperar()">¿Olvidó tu contraseña?</a>
+                    <a href="registro.html" class="btn btn-link w-100 mt-3">Registrarse</a>
                 </form>
                 <div id="recuperarDiv" style="display: none;" class="mt-3">
                     <form action="backend/recuperar.php" method="POST">
@@ -29,7 +33,7 @@
                         <button type="submit" class="btn btn-warning w-100">Recuperar contraseña</button>
                     </form>
                 </div>
-                <a href="registro.html" class="btn btn-link w-100 mt-3">¿No tienes cuenta? Regístrate</a>
+                
             </div>
         </div>
     </div>
