b6a9010e7234665178ebe58fac2653368001de58
diegopereh <diegopereh@gmail.com>
2025-04-04 13:23:07 +0200

    ya_funciona

--- logout.php
+++ logout.php
@@ -1,10 +1,18 @@
 <?php
 session_start();
 
+// Destruir todas las variables de sesión
+$_SESSION = array();
+
 // Destruir la sesión
 session_destroy();
 
-// Redirigir al usuario a la página de login
-header('Location: login.php');
+// Eliminar la cookie de "Recordarme"
+if (isset($_COOKIE['remember_me'])) {
+    setcookie('remember_me', '', time() - 3600, '/', '', isset($_SERVER['HTTPS']), true);
+}
+
+// Redirigir al login
+header("Location: login.php");
 exit();
 ?>
